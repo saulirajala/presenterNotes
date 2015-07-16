5505b7f5aa516056f6cdaf9a1b06a5e04b0989d9
Sauli Rajala <sauli.rajala@valu.fi>
2015-07-16 20:24:46 +0300

    Add scrollspy to work in dropdown menus

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,32 +2,45 @@
 	<div class="container">
 		<div class="row">
 			<!-- navi-->
-			<nav class="col-sm-3" id="myScrollspy">
-				<ul class="nav nav-pills nav-stacked">
-					<li class="active"><a href="#rakkaus">Rakkauden kaksoiskäsky</a></li>
-					<li><a href="#veritie">Veritie</a></li>
-					<li><a href="#opetus">Opetus</a></li>
-					<!--
-					<li class="dropdown">
-					  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
-					  <ul class="dropdown-menu">
-						<li><a href="#section41">Section 4-1</a></li>
-						<li><a href="#section42">Section 4-2</a></li>                     
-					  </ul>
-					</li> -->
-				</ul>
-			</nav>
+			<div class="nav-wrapper">
+				<nav class="col-xs-3 col-md-3" id="myScrollspy">
+					<ul class="nav nav-pills nav-stacked">
+						<li class="active"><a href="#rakkaus">Rakkauden kaksoiskäsky Rakkauden kaksoiskäsky Rakkauden kaksoiskäsky Rakkauden kaksoiskäsky</a></li>
+						<li><a href="#veritie">Veritie</a></li>
+						<li><a href="#">Opetus</a></li>
+						<li  class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+								Dropdown <span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Opetus</a></li>
+								<li><a href="#opetus">Opetus</a></li>
+							</ul>
+						</li>
 
-
-
+						
+						
+						<!--
+						<li class="dropdown">
+						  <a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
+						  <ul class="dropdown-menu">
+							<li><a href="#section41">Section 4-1</a></li>
+							<li><a href="#section42">Section 4-2</a></li>                     
+						  </ul>
+		 filteri, joka kävisi contentin läpi ja tulostaisi ensin navin otsikoiden perusteella 
+		 
+						</li> -->
+					</ul>
+				</nav>
+			</div>
 			<!--content-->
-			<div class="col-sm-9">
+			<div class="col-xs-9 col-md-9">
 				<article <?php post_class(); ?> data-spy="scroll" data-target=".navbar">
 					<header>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 						<?php //get_template_part('templates/entry-meta'); ?>
 					</header>
-					<div class="col-md-8">
+					<div class="">
 						<div class="entry-content" data-spy="scroll" data-target=".navbar" data-offset="12">
 
 
