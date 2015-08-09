e3f2b76e0326ee4c15c2c7d9bc84eaca1ba059c0
Sauli Rajala <sauli.rajala@valu.fi>
2015-08-09 14:57:23 +0300

    Change how title ids are made. Works now with [section]-shortcodes

--- lib/extras.php
+++ lib/extras.php
@@ -38,66 +38,37 @@ add_filter( 'excerpt_more', __NAMESPACE__ . '\\excerpt_more' );
 $otsikot = array();
 
 // [section]My Section Title[/section]                                          
-function whit_section_shortcode( $atts, $title = null ) {
-	// $content is the title you have between your [section] and [/section] 
-	//$atts[0] <- otsikko hierarki 1/1.1/1.2/.../3.4/...
-	$id = urlencode( strip_tags( $title ) );
+function irajala_section_shortcode( $atts, $title = null ) {
 	
-	$id = str_replace(".","",$id);
-	$id = str_replace(",","",$id);
-	// strip_tags removes any formatting (like <em> etc) from the title.
-	// Then urlencode replaces spaces and so on.
-//	echo $atts[0];
 	global $otsikot;
-	if ( isset( $atts[ 0 ] ) ) {
-		$otsikot[] = array( $id, $title, $atts[ 0 ] );
-	} else {
+	
+	if ( isset( $atts[ 0 ] ) && isset( $atts[ 1 ] ) ) {
+		$id="title".$atts[ 0 ]."-".$atts[ 1 ];
+		$otsikot[] = array( $id, $title, true );
+	} elseif (isset( $atts[ 0 ] ) ) {
+		$id="title".$atts[ 0 ];
 		$otsikot[] = array( $id, $title );
-	}
-
-//	$whit_sections .= '<li><a href="#' . $id . '">' . $title . '</a></li>';
-
+	} 	
 	return '<span id="' . $id . '">' . $title . '</span>';
 }
 
-add_shortcode( 'section', __NAMESPACE__ . '\\whit_section_shortcode' );
+add_shortcode( 'section', __NAMESPACE__ . '\\irajala_section_shortcode' );
 
 // [section_navigation] //tulostaa navigaation
-function whit_section_navigation_shortcode( $atts, $title = null ) {
+function irajala_section_navigation_shortcode( $atts, $title = null ) {
 
 	global $otsikot;
-	$return_val = '<ul class="nav nav-pills nav-stacked" id="page-nav">'; //lisätään listan aloitus
-	//tässä pitäis tapahtua magic
-//	var_dump( $whit_secti	ons );
-//	$var = array {
-//		[ 0 ] => array {
-//			[ 0 ] => string(21) "Historiallinen+tausta" [ 1 ] => string(21) "Historiallinen tausta"
-//		} [ 1 ] => array() {
-//			[ 0 ] => string "Liittyy+ns.+rakkauden+kaksoisk%C3%A4skyyn" [ 1 ] => string(37) "Liittyy ns. rakkauden kaksoiskäskyyn"
-//		} [ 2 ] => array(2) {
-//			[ 0 ] => string(36) "Veritie%2C+pappi+ja+leevil%C3%A4inen" [ 1 ] => string(30) "Veritie, pappi ja leeviläinen"
-//		} [ 3 ] => array(2) {
-//			[ 0 ] => string(29) "Vertauksen+opetus+ja+sovellus" [ 1 ] => string(29) "Vertauksen opetus ja sovellus"
-//		}
-//	};
+	$return_val = '<ul class="nav nav-pills nav-stacked">'; //lisätään listan aloitus
 	
 	for ($i=0; $i<count($otsikot); $i++) {
-		if ( !isset( $otsikot[$i][ 2 ] ) ) { //jos ei ole alaotsikko
-			//tulostetaan <ul> jos eka
-			if ( $i>0 && isset( $otsikot[$i-1][ 2 ] ) ) {//jos edellinen oli alaotsikko => tulostetaan ul-loppu
-				$return_val .= '</ul></li>';
-			}else { //muuten tulostetaan li-loppu eli edellin oli h1-tason lopputagi
-				$return_val .= '</li>'; 
-			}
+		if ( ! $otsikot[$i][ 2 ]  ) { //jos ei ole alaotsikko
+			//tulostetaan </li> jos eka
+			$return_val .= '</li>'; 
+			
 			//tulostetaan h1-taso
 			$return_val .= '<li><a href="#' . $otsikot[$i][ 0 ] . '">' . $otsikot[$i][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
-//			$return_val .= '</li>'; //lisätään listan itemin lopetus
 		} else { //jos on alaotsikko
-			//tulostetaan <ul> jos eka
-			if ( ! isset( $otsikot[$i-1][ 2 ] ) ) { //jos edellinen ei ollut alaotsikko
-				$return_val .= '<ul class="nav subnav">';
-			}
-			$return_val .= '<li><a href="#' . $otsikot[$i][ 0 ] . '">' . $otsikot[$i][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
+			$return_val .= '<li class="submenu"><a href="#' . $otsikot[$i][ 0 ] . '">' . $otsikot[$i][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
 		}
 		
 	}
@@ -105,5 +76,5 @@ function whit_section_navigation_shortcode( $atts, $title = null ) {
 	return $return_val;
 }
 
-add_shortcode( 'section_navigation', __NAMESPACE__ . '\\whit_section_navigation_shortcode' );
+add_shortcode( 'section_navigation', __NAMESPACE__ . '\\irajala_section_navigation_shortcode' );
 
--- templates/content-single.php
+++ templates/content-single.php
@@ -6,7 +6,6 @@
 				<article <?php post_class(); ?>>
 					<header>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
-						<?php //get_template_part('templates/entry-meta'); ?>
 					</header>
 					<div class="">
 						<div class="entry-content">
@@ -15,31 +14,13 @@
 							<?php the_content(); ?>
 						</div>
 					</div>
-
-					<?php //comments_template('/templates/comments.php'); ?>
 				</article>
 
 			</div>
 
 			<nav class="col-xs-3 col-md-3" id="myScrollspy">
-				<ul class="nav nav-pills nav-stacked">
-					<li class="active"><a href="#title1">Jumalakuvat</a></li>
-					<li class="submenu"><a href="#title1_1">Vihainen ja ankara</a></li>
-					<li class="submenu"><a href="#title1_2">Armoton valloittaja</a></li>
-					<li ><a href="#title2">Fil. 2:5-11</a></li>
-					<li class="submenu"><a href="#title2_1">“Tyhjensi itsensä”</a></li>
-					<li class="submenu"><a href="#title2_2">Jutun hienous</a></li>
-					<li><a href="#title3">So what?</a></li>
-					<li class="submenu"><a href="#title3_1">Ajattele positiivisemmin itsestäsi</a></li>
-					<li class="submenu"><a href="#title3_2">Ajattele positiivisemmin nykyisestä maailmanmenosta</a></li>
-					<li class="submenu"><a href="#title3_3">Ajattele positiivisemmin tulevaisuudesta</a></li>
-				</ul>
 				<?php
-				/*
-				 * Ongelma on nyt se, että menu tulee automaattisesti, mutta ilmeisesti koska id:t ovat niin erikoisia, niin scrollspy ei toimi
-				 * => pitäisi siis saada id:t selkeämmäksi tyyliin title1_1, tms. Erikoismerkit (+,&,",yms) ja skandinaavit tuovat vaikeutta
-				 */
-				//echo do_shortcode( "[section_navigation]" ); 
+				echo do_shortcode( "[section_navigation]" ); 
 				?>
 			</nav>
 		</div>
