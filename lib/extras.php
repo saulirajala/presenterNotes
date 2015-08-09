44f31730a6d6e5c533d7569ab532f526406c1908
Sauli Rajala <sauli.rajala@valu.fi>
2015-08-09 15:33:38 +0300

    Fix shortcode to follow shortcode API

--- lib/extras.php
+++ lib/extras.php
@@ -37,18 +37,23 @@ add_filter( 'excerpt_more', __NAMESPACE__ . '\\excerpt_more' );
 
 $otsikot = array();
 
-// [section]My Section Title[/section]                                          
+// [section title="1" subtitle="1"]My Section Title[/section]                                          
 function irajala_section_shortcode( $atts, $title = null ) {
-	
+
 	global $otsikot;
-	
-	if ( isset( $atts[ 0 ] ) && isset( $atts[ 1 ] ) ) {
-		$id="title".$atts[ 0 ]."-".$atts[ 1 ];
-		$otsikot[] = array( $id, $title, true );
-	} elseif (isset( $atts[ 0 ] ) ) {
-		$id="title".$atts[ 0 ];
-		$otsikot[] = array( $id, $title );
-	} 	
+	$atts = shortcode_atts(
+	array(
+		'title'		 => null,
+		'subtitle'	 => null,
+	), $atts, 'irajala_section_shortcode' );
+	var_dump( $atts );
+	if ( isset( $atts[ 'title'  ] ) && isset( $atts[ 'subtitle' ] ) ) {
+		$id			 = "title" . $atts[ 'title' ] . "-" . $atts[ 'subtitle' ];
+		$otsikot[]	 = array( $id, $title, true );
+	} elseif ( isset( $atts[ 'title' ] ) ) {
+		$id			 = "title" . $atts[ 'title' ];
+		$otsikot[]	 = array( $id, $title );
+	}
 	return '<span id="' . $id . '">' . $title . '</span>';
 }
 
@@ -59,18 +64,17 @@ function irajala_section_navigation_shortcode( $atts, $title = null ) {
 
 	global $otsikot;
 	$return_val = '<ul class="nav nav-pills nav-stacked">'; //lisätään listan aloitus
-	
-	for ($i=0; $i<count($otsikot); $i++) {
-		if ( ! $otsikot[$i][ 2 ]  ) { //jos ei ole alaotsikko
+
+	for ( $i = 0; $i < count( $otsikot ); $i++ ) {
+		if ( !$otsikot[ $i ][ 2 ] ) { //jos ei ole alaotsikko
 			//tulostetaan </li> jos eka
-			$return_val .= '</li>'; 
-			
+			$return_val .= '</li>';
+
 			//tulostetaan h1-taso
-			$return_val .= '<li><a href="#' . $otsikot[$i][ 0 ] . '">' . $otsikot[$i][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
+			$return_val .= '<li><a href="#' . $otsikot[ $i ][ 0 ] . '">' . $otsikot[ $i ][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
 		} else { //jos on alaotsikko
-			$return_val .= '<li class="submenu"><a href="#' . $otsikot[$i][ 0 ] . '">' . $otsikot[$i][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
+			$return_val .= '<li class="submenu"><a href="#' . $otsikot[ $i ][ 0 ] . '">' . $otsikot[ $i ][ 1 ] . '</a>'; //lisätään listan itemin aloitus ja linkki
 		}
-		
 	}
 	$return_val .= '</ul>'; //lisätään listan lopetus
 	return $return_val;
